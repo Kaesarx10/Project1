7d584dc228c2548acb3432fbf09fff23d1fea298
kaesarx10 <venzor.ca@gmail.com>
2021-08-10 00:34:17 -0500

    c7

--- Project1.1.py
+++ Project1.1.py
@@ -9,70 +9,19 @@ import requests
 
 import pandas as pd
 
-
 from bs4 import BeautifulSoup as bs
-
 
 import csv
 
-
 import numpy as np
-
 
 from pandas import DataFrame as df
 
-
 import datetime
-
 
 yahoofin = 'https://finance.yahoo.com/quote/'
 
-
 appleinc = 'AAPL'
-
-
-y_aapl = requests.get(yahoofin+appleinc)
-y_aapl = y_aapl.text
-
-
-# In[11]:
-
-
-aapl_soup = bs(y_aapl, 'lxml')
-
-
-# In[12]:
-
-
-price = aapl_soup.find('div', attrs={'id': 'quote-header-info'}).find(attrs={'data-reactid':'49'})
-price = price.get_text()
-price
-
-
-# In[13]:
-
-
-aapl_table = pd.Series([price], index=[appleinc])
-aapl_table
-
-
-# In[15]:
-
-
-aapl_dframe = pd.DataFrame({appleinc:[price]}, index = [datetime.date.today()])  #np.arange(len(list(appleinc))
-aapl_dframe = aapl_dframe.transpose()
-aapl_dframe
-
-
-
-
-aapl_dframe.to_csv('aapl_2.csv')
-=
-
-pd.DataFrame({datetime.date.today():[price]}, index = [appleinc])
-
-stock = yahoofin+appleinc
-stock
 
 
 
@@ -81,7 +30,6 @@ def stock_price(stock_url: str)-> str:
     aapl_soup = bs(y_aapl, 'lxml')
     price = aapl_soup.find('div', attrs={'id': 'quote-header-info'}).find(attrs={'data-reactid':'49'}).get_text()
     return price
-
 
 
 sp = stock_price(stock)
